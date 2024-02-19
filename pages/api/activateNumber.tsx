cdfbd4ece4dc667bc5cc93195a715338934939f9
ArielG83 <gromanariel@gmail.com>
2024-02-19 14:47:02 +0200

    fix phoneNumber activation

--- pages/api/activateNumber.tsx
+++ pages/api/activateNumber.tsx
@@ -36,27 +36,6 @@ export default async function handler(
           res.status(500).end(`invalid phone number: ${body.phoneNumber}`);
         }
         try {
-          // const request = {
-          //   // headers: {
-          //   //   "Access-Control-Allow-Methods": `POST, OPTIONS`,
-          //   //   "Access-Control-Allow-Headers": `Content-Type, Authorization`,
-          //   //   "Access-Control-Allow-Origin": `*`,
-          //   // },
-          //   url: "https://wapp.callindex.co.il/c2c/open_numbers.php",
-          //   auth: {
-          //     username: "rutw@harel-ins.co.il",
-          //     password: "Harel7946",
-          //   },
-          //   method: 'post',
-          //   data: {
-          //     uberId: '3613',
-          //     phone_number: "033818324",
-          //     prefix: body.phoneNumber,
-          //   },
-          // }
-          // console.log(request, '----------------request-------------------------------------')
-          // const Response = await axios(request);
-
           const myHeaders = new Headers();
           myHeaders.append("Authorization", "Basic cnV0d0BoYXJlbC1pbnMuY28uaWw6SGFyZWw3OTQ2");
           myHeaders.append("Cookie", "incap_ses_1687_63307=eXp/DmbGHR/BEHo4vG1pF9YO02UAAAAAx1Q1Yezp0AaCdTtqeKRnlQ==; incap_ses_820_63307=MD1TenQJQUVOMVrW2DphCzYS02UAAAAAKP8tJ60EIzYndMQVkY8EWg==; nlbi_63307=kOQZO/5iBWZotSOZuh4RKgAAAADr42NF0EDQExxqYExBIOfL; visid_incap_63307=yLQRyUT/Qh2XYIHvFVyVRPVOw2UAAAAAQUIPAAAAAACY3gJ3veoqr7en2IpKDAzy; PHPSESSID=7603ebc761d6ed4013c6a10330aef5d8");
@@ -64,18 +43,17 @@ export default async function handler(
           const formdata = new FormData();
           formdata.append("uberId", "3613");
           formdata.append("phone_number", "033818324");
-          formdata.append("prefix", "972524453255");
+          formdata.append("prefix", body.phoneNumber);
 
-          const requestOptions = {
+          const Response = await fetch("https://wapp.callindex.co.il/c2c/open_numbers.php", {
             method: "POST",
             headers: myHeaders,
             body: formdata,
             redirect: "follow"
-          };
+          })
 
-          const Response = await fetch("https://wapp.callindex.co.il/c2c/open_numbers.php", requestOptions)
           if (Response) {
-            res.status(200).end(`activated number successfuly: ${body.phoneNumber}`);
+            res.status(200).end(`activated number successfuly: ${body.phoneNumber}, response ${JSON.stringify(Response)}`);
           }
           res.status(500).end(`failed to activate number: ${body.phoneNumber}`);
         } catch (err) {
